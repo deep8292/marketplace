1362753305dc096803bc9d047daa354432dce814
Deepak Khiwani <d.khiwani92@gmail.com>
2023-05-08 09:50:02 -0230

    code clean up

--- src/components/common/GridView.jsx
+++ src/components/common/GridView.jsx
@@ -4,27 +4,26 @@ import images from '../../helpers/images';
 function TitleView() {
     return (
         <div className="container">
-            {}
-            <div className="grid-item">
-                <div className='image-wrapper'>
+            <div>
+                <div>
                     <img className='image' src={images[0]}/>
                     <div className="text">Item 1</div>
                 </div>
             </div>
-            <div className="grid-item">
-                <div className='image-wrapper'>
+            <div>
+                <div>
                     <img className='image' src={images[0]}/>
                     <div className="text">Item 2</div>
                 </div>
             </div>
-            <div className="grid-item">
-                <div className='image-wrapper'>
+            <div>
+                <div>
                     <img className='image' src={images[0]}/>
                     <div className="text">Item 3</div>
                 </div>
             </div>
-            <div className="grid-item">
-                <div className='image-wrapper'>
+            <div>
+                <div>
                     <img className='image' src={images[0]}/>
                     <div className="text">Item 6</div>
                 </div>
