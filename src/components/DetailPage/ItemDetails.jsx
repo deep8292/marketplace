cf1879d5b0e1dd55981343cbdf77f759ac8893e9
Deepak Khiwani <d.khiwani92@gmail.com>
2023-06-28 19:36:40 -0230

    feat: added details page

--- src/components/DetailPage/ItemDetails.jsx
+++ src/components/DetailPage/ItemDetails.jsx
@@ -3,14 +3,23 @@ import { useLocation } from "react-router-dom";
 import HomeHeader from "../common/Header";
 import Footer from "../common/Footer";
 import ImageCarousel from "../common/ImageCarousel";
+import ItemDescription from "./ItemDescription";
+
+import classes from '../styles/ItemDetails.module.css';
+import UserDetails from "./UserDeatils";
 
 function ItemDetails() {
     let itemDetails = useLocation();
-    console.log(itemDetails.state);
     return(
         <>
         <HomeHeader />
         <ImageCarousel />
+        <div className={classes.container}>
+            <ItemDescription />
+            <UserDetails />
+            {/* <div style={{ width: '50%', height: '200px', backgroundColor: 'red' }}></div>
+            <div style={{ width: '50%', height: '200px', backgroundColor: 'blue' }}></div> */}
+        </div>
         <Footer />
         </>
     );
---
+++ src/components/DetailPage/UserDeatils.jsx
@@ -0,0 +1,16 @@
+import classes from '../styles/UserDescription.module.css';
+
+function UserDetails () {
+    return(
+        <div className={classes.boxContainer}>
+            <p className={classes.descriptionTitle}>User Name</p>
+            <p className={classes.descriptionDetails}>
+            Phone: +1 (777)-999-999<br />
+            Email: john@doe.com
+            </p>
+            
+        </div>
+    );
+}
+
+export default UserDetails;
