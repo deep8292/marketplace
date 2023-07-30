245460528269b06c53429247ef57e2ff4a15fb54
Deepak Khiwani <d.khiwani92@gmail.com>
2023-07-30 21:08:08 -0230

    fix: minor change

--- src/components/DetailPage/UserDeatils.jsx
+++ src/components/DetailPage/UserDeatils.jsx
@@ -1,14 +1,11 @@
 import classes from '../styles/UserDescription.module.css';
 
 function UserDetails ({itemInfo}) {
-    const phone = `Phone: + ${itemInfo.seller_info.phone}`;
-    console.log(phone);
     return(
         <div className={classes.boxContainer}>
             <p className={classes.descriptionTitle}>{itemInfo.seller_info.name}</p>
             <p className={classes.descriptionDetails}>
-                {phone}
-            <br />
+                Phone: {itemInfo.seller_info.phone}
             </p>
         </div>
     );
