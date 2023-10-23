315e4496e09c4ace30af7d436df3781db35fb2fc
Deepak Khiwani <d.khiwani92@gmail.com>
2023-10-23 20:32:03 -0230

    new: added cart functions to UserContext

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -3,10 +3,30 @@ import { createContext, useContext, useState } from 'react';
 const UserContext = createContext();
 
 export const UserProvider = ({children}) => {
-    const [loggedIn, setLoggedIn] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [cartCount, setCartCount] = useState(0);
+    const [cartItems, setCartItems] = useState([]);
+
+    const addToCart = (item) => {
+        setCartItems((prevCart) => [...prevCart, item]);
+        setCartCount((prevCount) => prevCount + 1);
+    };
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prevCart) => prevCart.filter((item) => item.id !== itemId));
+        setCartCount((prevCount) => prevCount - 1);
+    };
+
 
     return (
-        <UserContext.Provider value={{loggedIn, setLoggedIn}}>
+        <UserContext.Provider value={{
+                loggedIn, 
+                setLoggedIn,
+                cartCount,
+                cartItems,
+                addToCart,
+                removeFromCart,
+            }}>
             {children}
         </UserContext.Provider>
     );
