7f074bea759d412d1d3d246f833f49156d9fd1e7
Deepak Khiwani <d.khiwani92@gmail.com>
2023-11-05 15:00:08 -0330

    - removed lottiefiles package

--- src/components/OrderPlaced/OrderPlaced.jsx
+++ src/components/OrderPlaced/OrderPlaced.jsx
@@ -1,7 +1,5 @@
 import classes from './OrderPlaced.module.css';
-
-import Lottie from "lottie-react";
-import animationData from '../../helpers/animation_check_mark.json';
+import checkmark from '../../helpers/checkmark.png';
 import RetroButton, { RetroButtonType } from '../button/RetroButton';
 
 function OrderPlaced ({ goToHome }) {
@@ -21,7 +19,9 @@ function OrderPlaced ({ goToHome }) {
             <div className={classes.container}>
                 <p className={classes.titleConfig}>Thank you for placing the order</p>
                 <p className={classes.subTitleConfig}>You will recieve the confirmation and details on your email</p>
-                <Lottie animationData={animationData} loop={true} height={50} width={50} />
+                <div style={{padding: '2rem'}}>
+                    <img style={{width: '200px', height: '200px', justifyContent: 'center'}} src={checkmark}/>
+                </div>
                 <RetroButton 
                     buttonType={RetroButtonType.YELLOW}
                     onClick={onClickReturn}
